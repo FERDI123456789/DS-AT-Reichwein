0dfc0500c472a8bd9d3b2dd5069ad3751c97dfb0
FERDI123456789 <ferdi0909@protonmail.ch>
2024-10-17 15:52:41 -0700

    Fertig

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BprkRl0q.mjs';
-import { manifest } from './manifest_emvZwlF6.mjs';
+import { manifest } from './manifest_7boaqaqf.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
@@ -39,7 +39,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "e84832bf-738d-4858-be21-4a5d27b3a95f",
+    "middlewareSecret": "b362f82b-d0e2-4edb-aae1-77a074007d0d",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
