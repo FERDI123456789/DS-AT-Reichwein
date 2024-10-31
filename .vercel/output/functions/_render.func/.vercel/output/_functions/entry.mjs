4ed3fb4cdd29566c6dd264f142bf7958eab38a46
FERDI123456789 <ferdi0909@protonmail.ch>
2024-10-31 14:50:37 -0700

    Homifyss is Live!

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CImhRhEk.mjs';
-import { manifest } from './manifest_C0hJqOCY.mjs';
+import { manifest } from './manifest_usdKjyE5.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
@@ -39,7 +39,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "78d3d5eb-d66b-4fd3-a9a0-df304e4f269e",
+    "middlewareSecret": "f19fdefb-1c69-4abd-83a1-c63ecfcda3be",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
